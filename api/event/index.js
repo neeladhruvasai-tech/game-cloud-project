d1bac9c281c771e81657046e600b133fc4a00ce3
neeladhruvasai-tech <neeladhruvasai@gmail.com>
2026-07-01 19:00:06 -0400

    Implement event handling in index.js

--- api/event/index.js
+++ api/event/index.js
@@ -1,1 +1,13 @@
+module.exports = async function (context, req) {
+    const event = req.body;
+
+    context.log("Game event received:", event);
+
+    context.bindings.outputDocument = event;
+
+    context.res = {
+        status: 200,
+        body: { ok: true }
+    };
+};
 
