8fbe07f1f052a1b32d9a9f7554e2921a27f489fb
neeladhruvasai-tech <neeladhruvasai@gmail.com>
2026-07-01 19:01:37 -0400

    Update main.js

--- game/main.js
+++ game/main.js
@@ -1,1 +1,59 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: { default: 'arcade' },
+    scene: { preload, create, update }
+};
+
+let player;
+let stars;
+let score = 0;
+
+function preload() {
+    this.load.image('player', 'assets/player.png');
+    this.load.image('star', 'assets/star.png');
+}
+
+function create() {
+    player = this.physics.add.sprite(400, 300, 'player');
+
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 10,
+        setXY: { x: 12, y: 12, stepX: 70 }
+    });
+
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+}
+
+function update() {
+    const cursors = this.input.keyboard.createCursorKeys();
+
+    if (cursors.left.isDown) player.x -= 5;
+    if (cursors.right.isDown) player.x += 5;
+    if (cursors.up.isDown) player.y -= 5;
+    if (cursors.down.isDown) player.y += 5;
+}
+
+function collectStar(player, star) {
+    star.disableBody(true, true);
+    score += 10;
+    sendEvent("score", score);
+}
+
+function sendEvent(type, value) {
+    fetch(API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            type,
+            value,
+            timestamp: Date.now(),
+            playerId: "dhruva"
+        })
+    });
+}
+
+new Phaser.Game(config);
 
