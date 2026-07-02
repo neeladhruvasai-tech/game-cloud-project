8e82cf424740989359cc94043d9bc7c7af7055a6
neeladhruvasai-tech <neeladhruvasai@gmail.com>
2026-07-02 09:52:37 -0400

    Update config.js

--- game/config.js
+++ game/config.js
@@ -1,3 +1,3 @@
 // Replace this with your Azure Function URL later
-const API_URL = "https://YOUR-FUNCTION-NAME.azurewebsites.net/api/event";
+const API_URL = "https://wonderful-mushroom-08f1f9510.7.azurestaticapps.net";
 
