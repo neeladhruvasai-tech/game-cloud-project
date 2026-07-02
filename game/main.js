ed6fd7a37e3e9560ef0f54a83025935e1b6bf565
neeladhruvasai-tech <neeladhruvasai@gmail.com>
2026-07-02 11:21:13 -0400

    Update main.js

--- game/main.js
+++ game/main.js
@@ -2,12 +2,13 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    physics: { default: 'arcade' },
+    physics: { default: 'arcade', arcade: { gravity: { y: 0 } } },
     scene: { preload, create, update }
 };
 
 let player;
 let stars;
+let cursors;
 let score = 0;
 
 function preload() {
@@ -16,33 +17,38 @@ function preload() {
 }
 
 function create() {
-    // Temporary background so stars are visible
-    this.cameras.main.setBackgroundColor('#222222');
+    // Background so stars are visible
+    this.cameras.main.setBackgroundColor('#1a1a1a');
 
+    // Player
     player = this.physics.add.sprite(400, 300, 'player');
+    player.setCollideWorldBounds(true);
 
-    // Spawn stars in the middle of the screen
+    // Keyboard controls
+    cursors = this.input.keyboard.createCursorKeys();
+
+    // Stars (spawn in middle)
     stars = this.physics.add.group({
         key: 'star',
         repeat: 5,
         setXY: { x: 100, y: 300, stepX: 120 }
     });
 
-    stars.children.iterate(function (child) {
+    stars.children.iterate(child => {
         child.setBounceY(Phaser.Math.FloatBetween(0.2, 0.5));
     });
 
+    // Collision
     this.physics.add.overlap(player, stars, collectStar, null, this);
 }
 
-
 function update() {
-    const cursors = this.input.keyboard.createCursorKeys();
+    player.setVelocity(0);
 
-    if (cursors.left.isDown) player.x -= 5;
-    if (cursors.right.isDown) player.x += 5;
-    if (cursors.up.isDown) player.y -= 5;
-    if (cursors.down.isDown) player.y += 5;
+    if (cursors.left.isDown) player.setVelocityX(-200);
+    if (cursors.right.isDown) player.setVelocityX(200);
+    if (cursors.up.isDown) player.setVelocityY(-200);
+    if (cursors.down.isDown) player.setVelocityY(200);
 }
 
 function collectStar(player, star) {
@@ -65,4 +71,3 @@ function sendEvent(type, value) {
 }
 
 new Phaser.Game(config);
-
